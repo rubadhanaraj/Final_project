1a188f03387240eb84aac8c9c9745b1c499c66e9
AmbreenRafique <108497494+AmbreenRafique@users.noreply.github.com>
2022-11-23 15:01:28 -0600

    Add files via upload

---
+++ Queries.sql
@@ -0,0 +1,37 @@
+DROP TABLE Customer CASCADE;
+-- Creating tables for final project
+-- creating order table
+
+
+-- Creating product table 
+CREATE TABLE Product (
+     Order_ID VARCHAR  NOT NULL,
+     Product_ID varchar NOT NULL,
+	 Category varchar Not NULL,
+	 Sub_Category varchar Not NULL,
+	 Segment varchar NOT NULL    	 
+);
+
+-- Creating Shipping_info table 
+CREATE TABLE Shipping_info (
+     Order_ID varchar NOT NULL,
+	 City varchar NOT NULL,
+     States varchar NOT NULL,
+	 Country varchar NOT NULL,
+	 Shipping_Cost varchar NOT NULL,
+	 Order_Priority varchar NOT NULL	 
+);
+
+-- Creating customer table 
+CREATE TABLE Customer (
+     Customer_ID VARCHAR  NOT NULL,
+	 Customer_Name VARCHAR NOT NULL,
+     Segment VARCHAR NOT NULL
+);	
+
+CREATE TABLE sales (
+     Row_ID int NOT NULL,
+	 Sales VARCHAR NOT NULL,
+	 Quantity int NOT NULL,
+	 Profit VARCHAR NOT NULL
+);
